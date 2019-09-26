b8f2238a2c41f9f5dda2bc4ca70d28d3b82a8748
inamasu <46822887+inamasuJP@users.noreply.github.com>
2019-09-27 02:33:23 +0900

    Change build path

--- Scripts/Render.py
+++ Scripts/Render.py
@@ -4,8 +4,9 @@ bpy.context.scene.render.resolution_x = 1920
 bpy.context.scene.render.resolution_y = 1080
 bpy.context.scene.render.resolution_percentage = 25
 
-bpy.data.scenes['Scene'].render.engine = 'CYCLES'
-bpy.data.scenes["Scene"].render.filepath = "../../"+ bpy.path.basename(bpy.context.blend_data.filepath).split(".")[0] +".png"
+#bpy.data.scenes['Scene'].render.engine = 'CYCLES'
+path = bpy.context.blend_data.filepath.rstrip(bpy.path.basename(bpy.context.blend_data.filepath)) + "../image/" +  bpy.path.basename(bpy.context.blend_data.filepath).split(".")[0] +".png"
+bpy.data.scenes["Scene"].render.filepath = path
 
 bpy.ops.render.render( write_still=True )
 
