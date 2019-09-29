210339459bb6f5288c5692583100fa96c257d1d5
inamasu <46822887+inamasuJP@users.noreply.github.com>
2019-09-29 15:15:30 +0900

    Finish write convert.sh

--- Scripts/convert.sh
+++ Scripts/convert.sh
@@ -4,21 +4,15 @@ log=$(pwd)/gitPush.sh.log
 
 echo "[$(date)] start" >> $log
 
-echo "cd git top level dir(" `git rev-parse --show-toplevel` ")"
+echo "[Messeage by convert.sh]cd git top level dir(" `git rev-parse --show-toplevel` ")"
 cd  `git rev-parse --show-toplevel`
 
-for raw in $(find . -name "*.blend" 2>/dev/null | grep ".blend") ; do
-	blend=${raw#*/}
-    echo "blend file is ${blend}"
-    echo "Start Render.py"
-    #blender --background ${blend} --python Scripts/Render.py
-    echo "Start ExportFBX.py"
-    blender --background ${blend} --python Scripts/ExportFBX.py
-	#echo "cp ${raw} -t ${fbx%/*}"
-	#if [[ ! -d "3DAssets-OnlyFBX/${fbx%/*}" ]] ; then
-	#	echo "mkdir 3DAssets-OnlyFBX/${fbx%/*}"
-	#	mkdir 3DAssets-OnlyFBX/${fbx%/*} | tee -a $log
-	#fi
-	#cp ${raw} -t 3DAssets-OnlyFBX/${fbx%/*} | tee -a $log
-	
+for raw in $(git log origin/develop..develop --stat | grep ".blend") ; do
+	if [ ${raw##*.} = "blend" ]; then
+		echo "[Messeage by convert.sh] blend file is ${raw}"
+		echo "[Messeage by convert.sh] Start Render.py"
+		blender --background ${raw} --python Scripts/Render.py
+		echo "[Messeage by convert.sh] Start ExportFBX.py"
+		blender --background ${raw} --python Scripts/ExportFBX.py
+	fi	
 done
